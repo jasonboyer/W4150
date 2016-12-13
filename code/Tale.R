5e314694436c25903ec606d056847ae058aba584
Jason Boyer <jasonboyer@gmail.com>
2016-12-13 06:52:54 -0500

    Increase list size

--- code/Tale.R
+++ code/Tale.R
@@ -4,6 +4,9 @@
 #
 #################################################################
 DoTale <- function() {
+  plotCount<-0
+  plots<-list(50)
+
   gspc<-ReadStock("gspc", monthly=TRUE)
   monthNumStrings=c("01","02","03","04","05","06",
                     "07","08","09","10","11","12")
